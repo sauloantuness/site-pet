c5b7d03ddc7c61785daa4a0516c97de35bb75c1b
Saulo Antunes <sauloantuness@gmail.com>
2016-08-04 07:43:35 -0300

    Cria formulário de inscrição

--- interpet/models.py
+++ interpet/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Inscricao(models.Model):
+	Q1 = models.CharField(max_length=1000)
+	Q2 = models.CharField(max_length=1000)
+	Q3 = models.CharField(max_length=1000)
+	Q4 = models.CharField(max_length=1000)
+	Q5 = models.CharField(max_length=1000, blank=True)
+	Q6 = models.CharField(max_length=1000)
+	Q7 = models.CharField(max_length=1000)
+	Q8 = models.CharField(max_length=1000)
+	Q9 = models.CharField(max_length=1000)
+	Q10 = models.CharField(max_length=1000)
+	Q11 = models.CharField(max_length=1000)
+	Q12 = models.CharField(max_length=1000)
+	Q13 = models.CharField(max_length=1000)
+	Q14 = models.CharField(max_length=1000)
+	Q15 = models.CharField(max_length=1000)
+	Q16 = models.CharField(max_length=1000)
+	Q17 = models.CharField(max_length=1000)
+	Q18 = models.CharField(max_length=1000)
+	Q19 = models.CharField(max_length=1000)
+	Q20 = models.CharField(max_length=1000)
+	data = models.DateTimeField(auto_now=True, editable=True)
+
+	def __str__(self):
+		return self.Q1 + " - " + self.data.strftime("%H:%M %d/%m/%Y")
+
--- interpet/urls.py
+++ interpet/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.interpet),
-	url(r'^inscricao/', views.apply),
-
+	url(r'^inscricao/', views.inscricao),
 ]
