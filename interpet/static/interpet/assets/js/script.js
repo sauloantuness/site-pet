7c99c53ea25d355297c55719d1b25d230e132272
Saulo Antunes <sauloantuness@gmail.com>
2016-08-07 11:41:38 -0300

    Atualização informações dos eventos

--- interpet/static/interpet/assets/js/script.js
+++ interpet/static/interpet/assets/js/script.js
@@ -2,95 +2,101 @@ day1 = [
 	{
 		'start' : '8:00',
 		'end' : '9:00',
-		'description' : 'Abertura Diretoria Geral e Diretoria de Graduação.',
+		'description' : 'Credenciamento <br> Abertura: Diretoria Geral e Diretoria de Graduação',
+		'place' : 'Campus I - Auditório'
+	},
+	{
+		'start' : '9:00',
+		'end' : '9:30',
+		'description' : 'COFFEE BREAK',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '9:30',
 		'end' : '12:00',
-		'description' : 'Mesa de abertura: A importância do PET para a indissociabilidade entre ensino, pesquisa e extensão - Francisco José Gomes (Professor Titular e Tutor do Programa de Educação Tutorial do curso de Engenharia Elétrica - UFJF), Sandra Isabel dos Reis (Secretária do NAPq, do CENEX, do Sistema de Bolsas da FACE e do PET Administração e Economia - UFMG)',
+		'description' : 'Mesa de abertura: A importância do PET para a indissociabilidade entre ensino, pesquisa e extensão <br> <b>Francisco José Gomes</b> (Professor Titular e Tutor do Programa de Educação Tutorial do curso de Engenharia Elétrica - UFJF) <br> <b>Sandra Isabel dos Reis</b> (Secretária do NAPq, do CENEX, do Sistema de Bolsas da FACE e do PET Administração e Economia - UFMG)',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '12:00',
 		'end' : '14:00',
-		'description' : 'Intervalo - Almoço.',
+		'description' : 'Intervalo - Almoço',
 		'place' : ''
 	},
 	{
 		'start' : '14:00',
 		'end' : '16:00',
-		'description' : 'Relatos de experiência dos Grupos PET CEFET-MG: tutores e discentes.',
+		'description' : 'Relatos de experiência dos Grupos PET CEFET-MG: tutores e discentes',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '16:00',
 		'end' : '16:30',
-		'description' : 'Coffe Break.',
+		'description' : 'COFFEE BREAK',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '16:30',
 		'end' : '18:00',
-		'description' : 'Relatos de experiência dos Grupos PET CEFET-MG: tutores e discentes.',
+		'description' : 'Relatos de experiência dos Grupos PET CEFET-MG: tutores e discentes',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '18:00',
 		'end' : '',
-		'description' : 'Confraternização.',
+		'description' : 'Confraternização',
 		'place' : 'Local a definir'
 	},
 ]
 
 day2 = [
 	{
-		'start' : '9:00',
+		'start' : '8:30',
 		'end' : '10:00',
-		'description' : 'Mesa: O PET – Avaliações e Perspectivas - Igor Mota Morici (Presidente do Comitê Local de Acompanhamento e Avaliação – CLAA – do PET e Professor do Departamento de Ciências Sociais e Filosofia - CEFET-MG) ; Lindolpho Oliveira de Araújo Júnior (Professor no curso de Engenharia de Controle e Automação e Tutor PET Controle e Automação, Campus Leopoldina - CEFET-MG).',
+		'description' : 'Mesa: O PET – Avaliações e Perspectivas <br> <b>Igor Mota Morici</b> (Presidente do Comitê Local de Acompanhamento e Avaliação – CLAA – do PET e Professor do Departamento de Ciências Sociais e Filosofia - CEFET-MG) <br> <b>Lindolpho Oliveira de Araújo Júnior</b> (Professor no curso de Engenharia de Controle e Automação e Tutor PET Controle e Automação, Campus Leopoldina - CEFET-MG)',
+		'place' : 'Campus I - Auditório'
+	},
+	{
+		'start' : '10:00',
+		'end' : '10:30',
+		'description' : 'COFFEE BREAK',
+		'place' : 'Campus I - Auditório'
+	},
+	{
+		'start' : '10:30',
+		'end' : '12:00',
+		'description' : 'Reflexões Petianas: encontro de discentes',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '10:00',
 		'end' : '12:00',
-		'description' : 'Reflexões Petianas: discentes.',
-		'place' : 'Campus I - Auditório'
-	},
-	{
-		'start' : '10:00',
-		'end' : '12:00',
-		'description' : 'Reflexões Petianas: tutores.',
+		'description' : 'Reflexões Petianas: encontro de tutores',
 		'place' : 'Campus I - Sala'
 	},
 	{
 		'start' : '12:00',
 		'end' : '14:00',
-		'description' : 'Intervalo - Almoço.',
+		'description' : 'Intervalo - Almoço',
 		'place' : ''
 	},
 	{
 		'start' : '14:00',
 		'end' : '15:30',
-		'description' : 'Desdobramentos do PET: apresentações de petianos egressos.',
+		'description' : 'Desdobramentos do PET: apresentações de petianos egressos',
 		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '15:30',
 		'end' : '16:00',
-		'description' : 'Coffe Break.',
+		'description' : 'COFFEE BREAK',
 		'place' : ''
 	},
 	{
 		'start' : '16:00',
 		'end' : '17:30',
-		'description' : 'Encerramento: Elaboração de um documento de diretrizes do PET para os próximos anos.',
+		'description' : 'Encerramento: Elaboração de um documento de diretrizes do PET para os próximos anos',
 		'place' : 'Campus I - Auditório'
-	},
-	{
-		'start' : '18:00',
-		'end' : '',
-		'description' : 'Confraternização.',
-		'place' : 'Local a definir'
 	}
 ]
 
@@ -106,7 +112,7 @@ function load_schedule(json, element) {
 		} else {
 			$('<div class="page-header"> \
 				<h3> \
-					A partir de '+i.start+' \
+					A partir das '+i.start+' \
 					<small>'+i.place+'</small> \
 				</h3> \
 				<p>'+i.description+'</p></div>').appendTo(element)
