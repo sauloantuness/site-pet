34c20e00f98ff4dd13ec977073420c2d34d81ea9
Saulo Antunes <sauloantuness@gmail.com>
2016-08-07 11:10:58 -0300

    Alteração das informações do evento

--- interpet/static/interpet/assets/js/script.js
+++ interpet/static/interpet/assets/js/script.js
@@ -3,13 +3,13 @@ day1 = [
 		'start' : '8:00',
 		'end' : '9:00',
 		'description' : 'Abertura Diretoria Geral e Diretoria de Graduação.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
-		'start' : '9:00',
+		'start' : '9:30',
 		'end' : '12:00',
-		'description' : 'Mesa de abertura: A importância do PET para a indissociabilidade entre ensino, pesquisa e extensão - Sandra Isabel dos Reis (Secretária do NAPq, do CENEX, do Sistema de Bolsas da FACE e do PET Administração e Economia - UFMG).',
-		'place' : 'Campus 1 - Auditório'
+		'description' : 'Mesa de abertura: A importância do PET para a indissociabilidade entre ensino, pesquisa e extensão - Francisco José Gomes (Professor Titular e Tutor do Programa de Educação Tutorial do curso de Engenharia Elétrica - UFJF), Sandra Isabel dos Reis (Secretária do NAPq, do CENEX, do Sistema de Bolsas da FACE e do PET Administração e Economia - UFMG)',
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '12:00',
@@ -21,19 +21,19 @@ day1 = [
 		'start' : '14:00',
 		'end' : '16:00',
 		'description' : 'Relatos de experiência dos Grupos PET CEFET-MG: tutores e discentes.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '16:00',
 		'end' : '16:30',
 		'description' : 'Coffe Break.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '16:30',
 		'end' : '18:00',
 		'description' : 'Relatos de experiência dos Grupos PET CEFET-MG: tutores e discentes.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '18:00',
@@ -48,19 +48,19 @@ day2 = [
 		'start' : '9:00',
 		'end' : '10:00',
 		'description' : 'Mesa: O PET – Avaliações e Perspectivas - Igor Mota Morici (Presidente do Comitê Local de Acompanhamento e Avaliação – CLAA – do PET e Professor do Departamento de Ciências Sociais e Filosofia - CEFET-MG) ; Lindolpho Oliveira de Araújo Júnior (Professor no curso de Engenharia de Controle e Automação e Tutor PET Controle e Automação, Campus Leopoldina - CEFET-MG).',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '10:00',
 		'end' : '12:00',
 		'description' : 'Reflexões Petianas: discentes.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '10:00',
 		'end' : '12:00',
 		'description' : 'Reflexões Petianas: tutores.',
-		'place' : 'Campus 1 - Sala'
+		'place' : 'Campus I - Sala'
 	},
 	{
 		'start' : '12:00',
@@ -72,7 +72,7 @@ day2 = [
 		'start' : '14:00',
 		'end' : '15:30',
 		'description' : 'Desdobramentos do PET: apresentações de petianos egressos.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '15:30',
@@ -84,7 +84,7 @@ day2 = [
 		'start' : '16:00',
 		'end' : '17:30',
 		'description' : 'Encerramento: Elaboração de um documento de diretrizes do PET para os próximos anos.',
-		'place' : 'Campus 1 - Auditório'
+		'place' : 'Campus I - Auditório'
 	},
 	{
 		'start' : '18:00',
