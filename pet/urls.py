f8ae3d38f0d3302fc4daebd4f20f35886ba9b62d
Higor Coimbra <higor1308@gmail.com>
2016-08-10 21:55:59 -0300

    Model das notícias e urls alteradas

--- pet/urls.py
+++ pet/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^interpet/', include('interpet.urls')),
+    url(r'^website/', include('website.urls')),
 	url(r'^$', RedirectView.as_view(url='/interpet')),
 ]
--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
 # Create your models here.
+class User(models.Model):
+	pass	
+
+class Tag(models.Model):
+	name = models.CharField(max_length=200)
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	#cover = models.ImageField()
+	user = models.ForeignKey('User')
+	tags = models.ManyToManyField(Tag, related_name='posts')
+
+
+	created = models.DateTimeField(auto_now_add=True,auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False,auto_now=True)
+
